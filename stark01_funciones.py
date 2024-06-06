33900406e500a012124e70ca2b02fcaddbe3a147
Osorio Brenda <patituchi59@gmail.com>
2024-06-06 19:28:18 -0300

    arreglar los puntos comentados y terminar

--- stark01_funciones.py
+++ stark01_funciones.py
@@ -15,10 +15,7 @@ Profesores: Christian Baus,
 
 
 
-K. Determinar cuántos superhéroes tienen cada tipo de color de pelo.
-L. Determinar cuántos superhéroes tienen cada tipo de inteligencia (En caso de
-no tener, Inicializarlo con ‘No Tiene’).
-M. Listar todos los superhéroes agrupados por color de ojos.
+
 N. Listar todos los superhéroes agrupados por color de pelo.
 O. Listar todos los superhéroes agrupados por tipo de inteligencia 
 """
@@ -171,7 +168,7 @@ indicadores anteriores (ítems C a F)"""
 
 """J. Determinar cuántos superhéroes tienen cada tipo de color de ojos."""
 
-def agrupamiento_por_clave(lista: list, clave: str) -> int:
+def agrupamiento_por_ojos(lista: list, clave: str) -> int:
     cantidad = len(lista)
     heroes_ojos_marrones = 0
     heroes_ojos_amarillos = 0
@@ -207,21 +204,126 @@ def agrupamiento_por_clave(lista: list, clave: str) -> int:
         elif lista[i][clave] == "Hazel":
             heroes_ojos_hazel = heroes_ojos_hazel + 1    
     
-    print(heroes_ojos_marrones)
-    print(heroes_ojos_amarillos)
-    print(heroes_ojos_azules)
-    print(heroes_ojos_verdes)
-    print(heroes_ojos_amarillos_variacion)
-    print(heroes_ojos_plateados)
-    print(heroes_ojos_rojos)
-    print(heroes_ojos_hazel)
+    print(f"{heroes_ojos_marrones} tienen los ojos marrones")
+    print(f"{heroes_ojos_amarillos} tienen los oos amarillos")
+    print(f"{heroes_ojos_azules} tinenen los ojos azules")
+    print(f"{heroes_ojos_verdes} tinen los ojos verdes")
+    print(f"{heroes_ojos_amarillos_variacion} tienen los ojos amarillos sin iris")
+    print(f"{heroes_ojos_plateados} tienen los ojos plateados")
+    print(f"{heroes_ojos_rojos} tienen los ojos rojos")
+    print(f"{heroes_ojos_hazel} tienen los ojos Hazel")
             
     
-print(agrupamiento_por_clave(lista_personajes, "color_ojos"))
+#print(agrupamiento_por_clave(lista_personajes, "color_ojos"))
+
+
+"K. Determinar cuántos superhéroes tienen cada tipo de color de pelo."
+
+# def agrupamioento_por_pelo(lista:list, clave:str):
+    # heroes_pelo_marron = 0
+    # heroes_pelo_negro = 0
+    # heroes_pelo_blanco = 0
+
+    # cantidad = len(lista)
+    # heroes_ojos_marrones = 0
+    # heroes_ojos_amarillos = 0
+    # heroes_ojos_azules = 0
+    # heroes_ojos_verdes = 0
+    # heroes_ojos_amarillos_variacion = 0
+    # heroes_ojos_plateados = 0
+    # heroes_ojos_rojos = 0
+    # heroes_ojos_hazel = 0
+
+    # for i in range(cantidad):
+    #     if lista[i][clave] == "Brown":
+    #         heroes_ojos_marrones = heroes_ojos_marrones + 1
+            
+    #     elif lista[i][clave] == "Yellow":
+    #         heroes_ojos_amarillos = heroes_ojos_amarillos + 1 
+            
+    #     elif lista[i][clave] == "Blue" or lista[i][clave] == "blue":
+    #         heroes_ojos_azules = heroes_ojos_azules + 1
+        
+    #     elif lista[i][clave] == "Green":
+    #         heroes_ojos_verdes = heroes_ojos_verdes + 1
+            
+    #     elif lista[i][clave] == "Yellow (without irises)":
+    #         heroes_ojos_amarillos_variacion = heroes_ojos_amarillos_variacion + 1
+            
+    #     elif lista[i][clave] == "Silver":
+    #         heroes_ojos_plateados = heroes_ojos_plateados + 1
+
+    #     elif lista[i][clave] == "Red":
+    #         heroes_ojos_rojos = heroes_ojos_rojos + 1
+
+    #     elif lista[i][clave] == "Hazel":
+    #         heroes_ojos_hazel = heroes_ojos_hazel + 1    
+    
+    # print(f"{heroes_ojos_marrones} tienen los ojos marrones")
+    # print(f"{heroes_ojos_amarillos} tienen los oos amarillos")
+    # print(f"{heroes_ojos_azules} tinenen los ojos azules")
+    # print(f"{heroes_ojos_verdes} tinen los ojos verdes")
+    # print(f"{heroes_ojos_amarillos_variacion} tienen los ojos amarillos sin iris")
+    # print(f"{heroes_ojos_plateados} tienen los ojos plateados")
+    # print(f"{heroes_ojos_rojos} tienen los ojos rojos")
+    # print(f"{heroes_ojos_hazel} tienen los ojos Hazel")
+
+    #negros, pelados, rubios, marron y negro, nada, verde, blanco, marron, rojo y naranja, aurburn, amarillo
 
 
 
+"L. Determinar cuántos superhéroes tienen cada tipo de inteligencia (En caso de"
+"no tener, Inicializarlo con ‘No Tiene’)."
 
 
+# def agrupamiento_por_inteligencia(lista:list, clave:str):
+     # cantidad = len(lista)
+    # heroes_ojos_marrones = 0
+    # heroes_ojos_amarillos = 0
+    # heroes_ojos_azules = 0
+    # heroes_ojos_verdes = 0
+    # heroes_ojos_amarillos_variacion = 0
+    # heroes_ojos_plateados = 0
+    # heroes_ojos_rojos = 0
+    # heroes_ojos_hazel = 0
+
+    # for i in range(cantidad):
+    #     if lista[i][clave] == "Brown":
+    #         heroes_ojos_marrones = heroes_ojos_marrones + 1
+            
+    #     elif lista[i][clave] == "Yellow":
+    #         heroes_ojos_amarillos = heroes_ojos_amarillos + 1 
+            
+    #     elif lista[i][clave] == "Blue" or lista[i][clave] == "blue":
+    #         heroes_ojos_azules = heroes_ojos_azules + 1
+        
+    #     elif lista[i][clave] == "Green":
+    #         heroes_ojos_verdes = heroes_ojos_verdes + 1
+            
+    #     elif lista[i][clave] == "Yellow (without irises)":
+    #         heroes_ojos_amarillos_variacion = heroes_ojos_amarillos_variacion + 1
+            
+    #     elif lista[i][clave] == "Silver":
+    #         heroes_ojos_plateados = heroes_ojos_plateados + 1
+
+    #     elif lista[i][clave] == "Red":
+    #         heroes_ojos_rojos = heroes_ojos_rojos + 1
+
+    #     elif lista[i][clave] == "Hazel":
+    #         heroes_ojos_hazel = heroes_ojos_hazel + 1    
+    
+    # print(f"{heroes_ojos_marrones} tienen los ojos marrones")
+    # print(f"{heroes_ojos_amarillos} tienen los oos amarillos")
+    # print(f"{heroes_ojos_azules} tinenen los ojos azules")
+    # print(f"{heroes_ojos_verdes} tinen los ojos verdes")
+    # print(f"{heroes_ojos_amarillos_variacion} tienen los ojos amarillos sin iris")
+    # print(f"{heroes_ojos_plateados} tienen los ojos plateados")
+    # print(f"{heroes_ojos_rojos} tienen los ojos rojos")
+    # print(f"{heroes_ojos_hazel} tienen los ojos Hazel")
 
 
+"M. Listar todos los superhéroes agrupados por color de ojos."
+
+
+def heroes_de_cada_tipo_de_ojos():
+    pass
